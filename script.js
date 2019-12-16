900e874cc2328d386b2f51ac3f008ddbd83da313
Jomain Rouser <jomainrouser12@gmail.com>
2019-12-15 21:16:32 -0600

    added the variables and some of the javascript functions needed to run the password generator

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var pLength = 0
+var pSpecial = Boolean
+var pLower = Boolean
+var pUpper = Boolean
+var pNumber = Boolean
+
+var pLength = prompt('How long is your password?',  '8-128')
+
+var pUpper = confirm('Would you like uppercase letters?')
+
+var pLower = confirm('Would you like lowercase letters?')
+
+var pNumber = confirm('Would you like numbers?')
+
+var pSpecial = confirm('Would you like special characters?')
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -21,3 +37,68 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
+
+
+
+generateBtn.addEventListener('newValue', () => {
+    const length = +pLength.value
+})
+    
+
+console.log(pLength)
+
+function generatePassword(hasLower, hasNumber, hasSpecial, hasUpper); {
+
+}.;
+
+ 
+//generator functions - http://www.net-comber.com/charset.html  example of perfect charset too the left
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLower());
+
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(getRandomUpper());
+
+
+function getRandomSpecial() {
+    const special = '!@#$%^&*(){}[]=<>/,.';
+    return special[Math.floor(Math.random() * special.length)];
+}
+
+console.log(getRandomSpecial());
+
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber());
+
+
+if (pLength > 7 && pLength < 129); 
+    
+// } else {
+    
+// }
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    special: getRandomSpecial,
+    number: getRandomNumber
+};
+
+
+
+const hasUpper = pUpper
+const hasLower = pLower
+const hasNumber = pNumber
+const hasSpecial = pSpecial
