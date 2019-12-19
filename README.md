a57d95ea68b80bbcbf09d2d36425feeb2431c7d1
Jomain Rouser <jomainrouser12@gmail.com>
2019-12-18 23:27:38 -0600

    finished the project

--- README.md
+++
@@ -1,4 +0,0 @@
-# password_Generator
-
-first commit: created the html css and javascript for the generator
-second commit: created the variables and starting setting up the the functions.
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var pLength = 0
-var pSpecial = Boolean
-var pLower = Boolean
-var pUpper = Boolean
-var pNumber = Boolean
+var upperCalc = ["Q","W","E","R","T","Y","U","I","O","P"]
+var lowerCalc = Boolean
+var numberCalc = Boolean
+var specialCalc = Boolean
+var returnValue = []
+var x = 0
+var fullPassword = []
 
 var pLength = prompt('How long is your password?',  '8-128')
 
@@ -14,8 +17,79 @@ var pNumber = confirm('Would you like numbers?')
 
 var pSpecial = confirm('Would you like special characters?')
 
-// Assignment Code
+console.log(pUpper,pLength,pLower,pNumber,pSpecial)
+
+const hasUpper = pUpper
+const hasLower = pLower
+const hasNumber = pNumber
+const hasSpecial = pSpecial
+
+
+
+
+// function generatePassword1() {
+//     if (hasUpper) {
+//     for (let i = 0; i <= pLength; i++) {
+    
+//         var x = Math.random() * 9 
+//        console.log(upperCalc[i])
+//     } 
+//     } else return("")}
+
+//     console.log(generatePassword1,upperCalc[x])
+
+// function generatePassword2(pLower) {
+//     if (hasLower) {
+//     for (let i = 0; i <= pLength; i++) {
+//     }   
+//     } else return("")}
+
+// function generatePassword3(pNumber) {
+//     if (hasNumber) {
+//     for (let i = 0; i <= pLength; i++) {
+//     }      
+//     } else return('')}
+
+// function generatePassword4(pSpecial) {
+//     if (hasSpecial) {
+//     for (let i = 0; i <= pLength; i++) {
+//     }    
+//     } else return("")}
+
+function generatePassword() {
+    const randomFuncs = [];
+
+    if (hasUpper == true) {
+        randomFuncs.push(getRandomUpper);
+    } 
+    if (hasLower == true); {
+        randomFuncs.push(getRandomLower);
+    }
+    if  (hasNumber == true); {
+        randomFuncs.push(getRandomNumber);
+    }
+    if  (hasSpecial == true); {
+        randomFuncs.push (getRandomSpecial);
+    }
+
+    let fullPassword = []
+    for (let i = 4; i < pLength; i++) {
+        fullPassword += randomFuncs[Math.floor(Math.random()*randomFuncs.length)](); 
+
+    }
+}
+generatePassword(); console.log(fullPassword)
+
+
+
+
+//Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function(event) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = fullPassword;
+    
+});
 
 
 // Write password to the #password input
@@ -23,10 +97,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+      //passwordText.value = returnValue;
+//   copyBtn.removeAttribute("disabled");
+//   copyBtn.focus();
 }
 
 function copyToClipboard() {
@@ -34,71 +107,49 @@ function copyToClipboard() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
 
 
 
-generateBtn.addEventListener('newValue', () => {
-    const length = +pLength.value
-})
+// generateBtn.addEventListener('newValue', () => {
+//     const length = +pLength.value
+// })
     
 
 console.log(pLength)
 
-function generatePassword(hasLower, hasNumber, hasSpecial, hasUpper); {
+// function generatePassword(hasLower, hasNumber, hasSpecial, hasUpper); {
 
-}.;
+// };
 
  
 //generator functions - http://www.net-comber.com/charset.html  example of perfect charset too the left
 
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    fullPassword.push (String.fromCharCode(Math.floor(Math.random() * 26) + 97));;
 }
 
-console.log(getRandomLower());
+
 
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    fullPassword.push (String.fromCharCode(Math.floor(Math.random() * 26) + 65));;
 }
 
-console.log(getRandomUpper());
+
 
 
 function getRandomSpecial() {
     const special = '!@#$%^&*(){}[]=<>/,.';
-    return special[Math.floor(Math.random() * special.length)];
+    fullPassword.push(special[Math.floor(Math.random() * special.length)]);;
 }
 
-console.log(getRandomSpecial());
+
 
 
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    fullPassword.push(String.fromCharCode(Math.floor(Math.random() * 10) + 48));;
 }
 
-console.log(getRandomNumber());
-
-
-if (pLength > 7 && pLength < 129); 
-    
-// } else {
-    
-// }
-
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    special: getRandomSpecial,
-    number: getRandomNumber
-};
-
-
-
-const hasUpper = pUpper
-const hasLower = pLower
-const hasNumber = pNumber
-const hasSpecial = pSpecial
